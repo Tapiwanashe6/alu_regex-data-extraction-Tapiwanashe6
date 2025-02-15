219336169d3302990c728dfebfa0a6a2efa28684
Tapiwanashe6 <t.marufu@alustudent.com>
2025-02-15 07:16:37 -0800

    Regular Expressions for extracting data

---
+++ regex-data-extractor.js
@@ -0,0 +1,11 @@
+// Regular Expressions for extracting data
+const patterns = {
+  emails: /\b[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,}\b/g,
+  phoneNumbers: /\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}/g,
+  urls: /https?:\/\/\S+/g,
+  creditCards: /\b\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}\b/g,
+  currency: /\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?/g,
+  time: /\b\d{1,2}:\d{2}\s?(?:AM|PM)?\b/g, 
+  htmlTags: /<\/?\w+.*?>/g,
+  hashtags: /#\w+/g
+};
